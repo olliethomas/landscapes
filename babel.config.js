1705aaed0da9158ff21650cfb5551fcf539c64c1
Oliver Thomas <1835492+olliethomas@users.noreply.github.com>
2022-07-26 13:09:51 +0100

    try and figure out e2e testing

--- babel.config.js
+++ babel.config.js
@@ -48,9 +48,6 @@ module.exports = function(api) {
     plugins: [
       'babel-plugin-macros',
       '@babel/plugin-syntax-dynamic-import',
-      ["@babel/plugin-proposal-class-properties", {loose: true}],
-      ["@babel/plugin-proposal-private-methods", {loose: true}],
-      ["@babel/plugin-proposal-private-property-in-object", {loose: true}],
       isTestEnv && 'babel-plugin-dynamic-import-node',
       '@babel/plugin-transform-destructuring',
       [
